119bb4c3d350a8089eb70c636e13a641f663401c
Zedwag <zedwag@gmail.com>
2019-10-19 02:01:30 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require ('express')
+
+const app = express()
+
+app.get('/', (req, res) => res.send('API running'))
+
+const Port = process.env.Port || 5000
+
+app.listen(Port, () => console.log(`Server started on port ${Port}`))
